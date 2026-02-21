444e9929eb1328aa77e35a7091d8130344a7eb45
sun33003 <127114580+sun33003@users.noreply.github.com>
2026-02-21 13:56:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,31 +9,14 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# 启用旧版Luci
-sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
-sed -i '/src-git.*openwrt-23/s/^/#/' feeds.conf.default
-sed -i '/src-git.*openwrt-24/s/^/#/' feeds.conf.default
-sed -i '/src-git.*openwrt-25/s/^/#/' feeds.conf.default
-
-# Add a feed source
-# git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-# svn co https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone --depth=1 https://github.com/fw876/helloworld
 
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-sed -i '2s/^#//' feeds.conf.default
-sed -i '3s/^/#/' feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
-# git clone -b lua https://github.com/sbwml/luci-app-alist package/alist
-# git clone https://github.com/rufengsuixing/luci-app-onliner package/onliner
+#sed -i '2s/^#//' feeds.conf.default
+#sed -i '3s/^/#/' feeds.conf.default
