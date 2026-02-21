8c6b31b95efe8c4fd2811c83ba045ef0d836f231
sun33003 <127114580+sun33003@users.noreply.github.com>
2026-02-21 13:20:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,9 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.222.1/g' package/base-files/files/bin/config_generate
+# 默认地址
+sed -i 's/192.168.1.1/192.168.222.8/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.222.8/g' package/base-files/luci/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
